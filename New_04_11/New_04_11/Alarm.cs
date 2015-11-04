8d676f6cb9c7cef033532da423f188b548f3aff2
mikhnjuk-mikola <mikhnjuk94@gmail.com>
2015-11-04 20:32:39 +0200

    04.11.15

---
+++ New_04_11/New_04_11/Alarm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Alarm
+    {
+        public Alarm(string name)
+        {
+            name = Name;
+        }
+        public string Name { get; private set; }
+        public static void Vvod()
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите время дзвонка");
+                Console.WriteLine("Формат ввода:ДД.ДД.ДД ВВ:ВВ:ВВ");
+                DateTime alarmTime;
+                if (DateTime.TryParse(Console.ReadLine(), out alarmTime))
+                {
+                    if (alarmTime < DateTime.Now)
+                    {
+                        Console.WriteLine("Не верний формат ввода будильника.Будильник не может бить в прошедшем времени");
+                        break;
+                    }
+                    Task.Factory.StartNew(() => Follow(alarmTime));
+                    Console.WriteLine("Будильник был успешно установлен на: " + alarmTime);
+                    break;
+                }
+            }
+        }
+        public static void Follow(DateTime alarmTime)
+        {
+
+            var alarmTimeLocal = alarmTime;
+            while (true)
+            {
+                if (DateTime.Now >= alarmTimeLocal)
+                {
+                    Console.Beep(1000, 3000);
+                    Console.WriteLine("ДЗВОНОК!!!!!");
+                    break;
+                }
+
+            }
+        }
+    }
+
+
+}
+
+
+
---
+++ New_04_11/New_04_11/Osnova.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    internal class Osnova
+    {
+
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("БУДИЛЬНИК");
+                Console.WriteLine("1-Добавить будильник");
+                Console.WriteLine("2-Удалить будильник");
+                Console.WriteLine("3-Просмотреть информацию о будильниках");
+                Console.WriteLine("4-Вихoд");
+                int actionNumber;
+                if (int.TryParse(Console.ReadLine(), out actionNumber))
+                {
+                    if (actionNumber == 1)
+                    {
+                        Alarm.Vvod();
+                    }
+                    if (actionNumber == 2)
+                    {
+                    }
+                    if (actionNumber == 3)
+                    {
+                    }
+                    if (actionNumber == 4)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Неверный ввод, повторите попытку");
+                }
+            }
+
+        }
+
+    }
+}
