2171486563a83c5fed309d3d1ad33f2e41322e0b
mikhnjuk-mikola <mikhnjuk94@gmail.com>
2015-11-05 17:21:57 +0200

    04_11_15

---
+++ New_04_11/New_04_11/Colections.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Threading;
+namespace ConsoleApplication1
+{   
+       class Collections
+        {
+           // Alarm[] alarms;
+           public List<Alarm> _alarm = new List<Alarm>();
+           
+             
+            }
+
+
+            /*public int Length
+            {
+                get { return alarms.Length; }
+            }
+
+            public Alarm this[int index]
+            {
+                get
+                {
+                    return alarms[index];
+                }
+                set
+                {
+                    alarms[index] = value;
+                }
+            }*/
+        }
+    
+
+      
+    
