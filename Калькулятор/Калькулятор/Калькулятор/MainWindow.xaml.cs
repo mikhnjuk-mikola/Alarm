c418147273d929d1ae149ceb0b63f58a55a2704a
mikhnjuk-mikola <mikhnjuk94@gmail.com>
2015-11-18 21:35:23 +0200

    Калькулятор

---
+++ Калькулятор/Калькулятор/Калькулятор/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Калькулятор
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //string leftop = ""; 
+        //string operation = ""; 
+        //string rightop = ""; 
+ 
+    
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+
+       
+    public  static void Digits(object sender, ExecutedRoutedEventArgs e)
+        {
+
+            string s = (string)((Button)e.OriginalSource).Content;
+           TextBox1.Text += s;
+            
+        }
+        //private void Button_Click_1(object sender, RoutedEventArgs e)
+        //{
+        //    TextBox1.Clear();
+        //}
+        
+    }
+    //public class Digit
+    //{
+    //    static Digit()
+    //    {
+    //        ClickDigitComand = new RoutedCommand("Digit", typeof(MainWindow));
+    //    }
+    //    public static RoutedCommand ClickDigitComand { get; set; }
+    //}
+}
+
---
+++ Калькулятор/Калькулятор/Калькулятор/MainWindowViewModel.cs
@@ -0,0 +1,119 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Controls;
+
+
+namespace Калькулятор
+{
+    public class MainWindowViewModel : DependencyObject
+    {
+        public double Value
+        {
+            get { return (double)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(double), typeof(MainWindowViewModel));
+
+
+        public MainWindowViewModel()
+        {
+            PlusCommand = new RelayCommand(PlusCommandExecute);
+            MultiplicetionCommand = new RelayCommand(MultiplicetionCommandExecute);
+            SubtractionCommand = new RelayCommand(SubtractionCommandExecute);
+            DivisionCommand = new RelayCommand(DivisionCommandExecute);
+            ProcentCommand = new RelayCommand(ProcentCommandExecute);
+            ResultCommand = new RelayCommand(ResultCommandExecute);
+            DigitCommand = new RelayCommand(DigitCommandExecute);
+
+
+        }
+
+        private void DigitCommandExecute(object obj)
+        {
+            
+           
+        }
+        private void ResultCommandExecute(object obj)
+        {
+            Value = 15;
+        }
+        private void ProcentCommandExecute(object obj)
+        {
+            Value=14;
+        }
+        private void DivisionCommandExecute(object obj)
+        {
+            Value = 13;
+        }
+        private void MultiplicetionCommandExecute(object obj)
+        {
+            Value = 11;
+        }
+        private void PlusCommandExecute(object obj)
+        {
+            Value = 10;
+        }
+        private void SubtractionCommandExecute(object obj)
+        {
+            Value = 12;
+        }
+        public RelayCommand ResultCommand
+        {
+            get { return (RelayCommand)GetValue(ResultCommandProperty); }
+            set { SetValue(ResultCommandProperty, value); }
+        }
+        public RelayCommand DigitCommand
+        {
+            get { return (RelayCommand)GetValue(DigitCommandProperty); }
+            set { SetValue(DigitCommandProperty, value); }
+        }
+        public RelayCommand ProcentCommand
+        {
+            get { return (RelayCommand)GetValue(ProcentCommandProperty); }
+            set { SetValue(ProcentCommandProperty, value); }
+        }
+        public RelayCommand PlusCommand
+        {
+            get { return (RelayCommand)GetValue(PlusCommandProperty); }
+            set { SetValue(PlusCommandProperty, value); }
+        }
+        public RelayCommand DivisionCommand
+        {
+            get { return (RelayCommand)GetValue(DivisionCommandProperty); }
+            set { SetValue(DivisionCommandProperty, value); }
+        }
+        public RelayCommand SubtractionCommand
+        {
+            get { return (RelayCommand)GetValue(SubtractionCommandProperty); }
+            set { SetValue(SubtractionCommandProperty, value); }
+        }
+        public RelayCommand MultiplicetionCommand
+        {
+            get { return (RelayCommand)GetValue(MultiplicetionCommandProperty); }
+            set { SetValue(MultiplicetionCommandProperty, value); }
+        }        
+        public static readonly DependencyProperty PlusCommandProperty =
+            DependencyProperty.Register("PlusCommand", typeof(RelayCommand), typeof(MainWindowViewModel));
+        public static readonly DependencyProperty MultiplicetionCommandProperty =
+            DependencyProperty.Register("MultiplicetionCommand", typeof(RelayCommand), typeof(MainWindowViewModel));
+        public static readonly DependencyProperty SubtractionCommandProperty =
+              DependencyProperty.Register("SubtractionCommand", typeof(RelayCommand), typeof(MainWindowViewModel));
+        public static readonly DependencyProperty DivisionCommandProperty =
+                DependencyProperty.Register("DivisionCommand", typeof(RelayCommand), typeof(MainWindowViewModel));
+
+        public static readonly DependencyProperty ProcentCommandProperty =
+               DependencyProperty.Register("ProcentCommand", typeof(RelayCommand), typeof(MainWindowViewModel));
+
+        public static readonly DependencyProperty ResultCommandProperty =
+              DependencyProperty.Register("ResultCommand", typeof(RelayCommand), typeof(MainWindowViewModel));
+        public static readonly DependencyProperty DigitCommandProperty =
+                DependencyProperty.Register("DigitCommand", typeof(RelayCommand), typeof(MainWindowViewModel));
+      
+
+
+              
+
+    }
+}
